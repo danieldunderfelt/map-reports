2c62d5fd4479b1be21c201f73a4c99c8ee2206c1
danieldunderfelt <daniel@developsuperpowers.com>
2018-07-06 14:11:01 +0300

    Use react-leaflet, make map work.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,48 +1,51 @@
 import * as React from 'react'
-import * as L from 'leaflet'
+import LeafletMap from 'react-leaflet/es/Map'
+import TileLayer from 'react-leaflet/es/TileLayer'
 import 'leaflet/dist/leaflet.css'
 import styled from 'styled-components'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 
 const Root = styled.div<any>`
   width: 100%;
-  max-height: 100%;
+  height: 100vh;
   overflow: hidden;
 `
 
+const MapComponent = styled(LeafletMap)`
+  width: 100%;
+  height: 100%;
+`
+
+const attribution = `Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
+<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
+Imagery © <a href="http://mapbox.com">Mapbox</a>`
+
+const url = 'https://digitransit-dev-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}{retina}.png'
+
+@observer
 class Map extends React.Component<any, any> {
-  mapRef = React.createRef()
-  leaflet = null
-
-  componentDidMount() {
-    this.leaflet = this.initializeMap()
-  }
-
-  initializeMap() {
-    const map = L
-      // @ts-ignore
-      .map(this.mapRef.current)
-      .setView([60.1689784, 24.9230033], 13)
-
-    L.tileLayer(
-      'https://digitransit-dev-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}{retina}.png',
-      {
-        maxZoom: 18,
-        tileSize: 512,
-        zoomOffset: -1,
-        attribution:
-          'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        retina: '@2x',
-        baseLayer: true,
-      },
-    ).addTo(map)
-
-    return map
-  }
+  @observable position = [60.1689784, 24.9230033]
+  @observable zoom = 13
 
   render() {
-    return <Root innerRef={this.mapRef} />
+    return (
+      <Root>
+        <MapComponent
+          center={this.position}
+          zoom={this.zoom}
+          minZoom={6}
+          maxZoom={25}>
+          <TileLayer
+            zoomOffset={-1}
+            tileSize={512}
+            attribution={attribution}
+            retina="@2x"
+            url={url}
+          />
+        </MapComponent>
+      </Root>
+    )
   }
 }
 
