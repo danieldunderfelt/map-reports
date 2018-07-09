e21e82f5da694981e258a6aafb81722971522a29
danieldunderfelt <daniel@developsuperpowers.com>
2018-07-09 15:08:02 +0300

    Some marker styling.

--- src/components/MarkerIcon.tsx
+++ src/components/MarkerIcon.tsx
@@ -5,16 +5,35 @@ import classnames from 'classnames'
 
 injectGlobal`
   .marker-icon {
-    width: 30px;
-    height: 30px;
-    background: blue; 
+    
+    &[style] {
+      width: 0 !important;
+      height: 0 !important;
+      margin-top: 0 !important;
+      margin-left: 0 !important;
+    } 
+    
+    &:before {
+      display: block;
+      position: absolute;
+      left: 50%;
+      bottom: 10px;
+      content: "";
+      width: 2rem;
+      height: 2rem;
+      border: 1px solid white;
+      background: blue;
+      box-shadow: 0 0 10px rgba(0,0,0,1);
+      border-radius: 50% 50% 0 50%;
+      transform: rotate(45deg) translateX(-50%);
+    }
    
-    &.focused {
+    &.focused:before {
       background: red;
     }
     
-    &.blurred {
-      opacity: 0.25;
+    &.blurred:before {
+      opacity: 0.33;
       background: blue;
     }
   }
